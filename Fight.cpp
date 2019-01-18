458ca25295b4227793c1308291dde0b1a6f2c239
Massfice <Massfice@users.noreply.github.com>
2019-01-18 23:49:55 +0100

    e1.0
    Błędy przy kompilacji. Do poprawy.

--- Fight.cpp
+++ Fight.cpp
@@ -3,16 +3,24 @@
 #include "header.h" //Do³¹czamy nag³ówek. Dziêki temu nag³ówek wystarczy zmodyfikowaæ raz i bêdzie mia³o to wp³yw na wszystkie pliki z nag³ówkiem.
 
 class Fight {
-	
-	private:
 		
-		int &liczba;
-	
 	public:
-		
-		Fight(int &liczba) : liczba(liczba){}
-		
-		void inc() {
-			liczba = liczba + 2;
+		void getReward(ItemList* & itemsOnTheGround) { //Otrzymywanie nagrody za wygran¹ walkê.
+			
+			Item* item = new Item;
+			item->id = 5;
+			item->slots = 25;
+			item->name = "Wielki Miecz";
+			
+			items->addItem(itemsOnTheGround,item);
+			
+			system("cls");
+			cout << "Ork defeated! Great sword was dropped.";
+			system("pause");
+			
+			//W tej wersji nie ma zaimplementowanej walki. Ork zawsze zostaje pokonany i upuszcza wielki miecz.
+			//Wielki miecz zostaje dodany do listy przedmiotów (il) le¿¹cych na ziemi i mo¿e zostaæ podniesiony przez gracza.
+			//W finanlej wersji powinna pojawiæ siê ca³a klasa odpowiedzialna za walkê.
+			//Metoda getReward() powinna byæ metod¹ prywatn¹, uruchamian¹ po wygraniu walki.
 		}
 };
--- main.cpp
+++ main.cpp
@@ -7,72 +7,95 @@ Items* items = new Items();
 
 #include "Fight.cpp"
 
-void refreshScreen(ItemList* il) {
+//void refreshScreen(ItemList* il) {
+//	system("cls");
+//	cout << "W - Go Down | S - Go Up | Q - Quit" << endl << endl;
+//	
+//	ItemList* cil = il; //Current ItemList - obecna pozycja na liscie
+//	items->seekBegin(il);
+//	ItemList* temp = il;
+//	do {
+//		if(il == cil) cout << " * " << il->item->name << endl;
+//		else cout << "   "  << il->item->name << endl;
+//		items->nextItem(il);
+//	} while(il != temp);
+//	
+//	cout << endl;
+//}
+
+void refreshScreen() {
 	system("cls");
-	cout << "W - Next Item | S - Previous Item | Q - Quit" << endl << endl;
-	
-	ItemList* temp = il;
-	do {
-		if(il == temp) cout << "[ " << il->item->name << " ]" << endl;
-		else cout << il->item->name << endl;
-		items->nextItem(il);
-	} while(il != temp);
-	
-	cout << endl;
+	cout << "I - Open Inventory | F - Fight with terrible Orc | Q - Quit" << endl << endl;
 }
 
 int main() {
 	
-	ItemList* il = NULL;
+	ItemList* itemsOnTheGround = NULL; //Przedmioty na ziemi.
 	
-	Item* item = new Item;
-	item->id = 0;
-	item->slots = 5;
-	item->name = "Miecz";
-	
-	items->addItem(il,item);
-	
-	item = new Item;
-	item->id = 1;
-	item->slots = 3;
-	item->name = "Tarcza";
-	
-	items->addItem(il,item);
-	
-	item = new Item;
-	item->id = 2;
-	item->slots = 7;
-	item->name = "Luk";
-	
-	items->addItem(il,item);
-	
-	item = new Item;
-	item->id = 3;
-	item->slots = 1;
-	item->name = "Mikstura";
-	
-	items->addItem(il,item);
-	
-	item = new Item;
-	item->id = 3;
-	item->slots = 1;
-	item->name = "Mikstura";
-	
-	items->addItem(il,item);
-	
-	items->seekBegin(il);
+	Fight* f = new Fight();
+//	
+//	Item* item = new Item;
+//	item->id = 0;
+//	item->slots = 5;
+//	item->name = "Miecz";
+//	
+//	items->addItem(il,item);
+//	
+//	item = new Item;
+//	item->id = 1;
+//	item->slots = 3;
+//	item->name = "Tarcza";
+//	
+//	items->addItem(il,item);
+//	
+//	item = new Item;
+//	item->id = 2;
+//	item->slots = 7;
+//	item->name = "Luk";
+//	
+//	items->addItem(il,item);
+//	
+//	item = new Item;
+//	item->id = 3;
+//	item->slots = 1;
+//	item->name = "Mikstura";
+//	
+//	items->addItem(il,item);
+//	
+//	item = new Item;
+//	item->id = 3;
+//	item->slots = 1;
+//	item->name = "Mikstura";
+//	
+//	items->addItem(il,item);
+//	
+//	items->seekBegin(il);
+//
+	refreshScreen();
+//
 
-	refreshScreen(il);
-	
-	char c;
-	bool isRunning = true;
+//	char c; //Zmienna przechwytuj¹ca znak z klawiatury.
+//	bool isRunning = true; //
+//	
+//	while(isRunning) {
+//		c = _getch();
+//		
+//		switch(c) {
+//			case 's': items->nextItem(il); refreshScreen(il); break;
+//			case 'w': items->prevItem(il); refreshScreen(il); break;
+//			case 'q': isRunning = false;
+//		}
+//	}
+		
+	char c; //Zmienna przechwytuj¹ca znak z klawiatury.
+	bool isRunning = true; //Pêtla bêdzie dzia³a³a dopóki ta zmienna bêdzie 'True'. Umo¿liwia to wyjœcie z pêtli i zakoñczenie programu.
 	
 	while(isRunning) {
 		c = _getch();
 		
 		switch(c) {
-			case 'w': items->nextItem(il); refreshScreen(il); break;
-			case 's': items->prevItem(il); refreshScreen(il); break;
+			case 'i': break;
+			case 'f': f->getReward(itemsOnTheGround); break;
 			case 'q': isRunning = false;
 		}
 	}
